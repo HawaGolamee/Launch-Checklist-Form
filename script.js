7e4df5ec5886a041ef1dcd896a2534ce54d833a5
HawaGolamee <73973675+HawaGolamee@users.noreply.github.com>
2020-12-07 23:42:21 -0600

    validation

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Write your JavaScript code here!
-window.
+window.addEventListener("load", function(){
+   let form = document.querySelector(form);
+   Form.addEventListener("submit", function (event){
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      for (let item of form)
+      if ()
+   });
+})
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
